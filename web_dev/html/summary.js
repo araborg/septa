f08e7892bdcff138a97e13a19525035345ef65d6
araborg <arababs2015@gmail.com>
2025-09-11 21:01:38 +0100

    septa training: 3

--- web_dev/html/summary.js
+++ web_dev/html/summary.js
@@ -14,4 +14,24 @@ the <tr> (table row) tag, and each row contains
 cells defined using either the <th> (table header) 
 or <td> (table data) tags.
 
+<table>
+    <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+        <th>Header 3</th>
+    </tr>
+
+    <tr>
+        <td>Row 1, Cell 1</td>
+        <td>Row 1, Cell 2</td>
+        <td>Row 1, Cell 3</td>
+    </tr>
+
+    <tr>
+        <td>Row 2, Cell 1</td>
+        <td>Row 2, Cell 2</td>
+        <td>Row 2, Cell 3</td>
+    </tr>
+</table>
+
 */
