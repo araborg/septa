c95d5b7949a7f8319063d416fa502a784049e8c0
araborg <arababs2015@gmail.com>
2025-09-11 20:59:09 +0100

    first commit

---
+++ web_dev/html/summary.js
@@ -0,0 +1,15 @@
+/*
+
+
+Width and Height: 
+Define the dimensions of the image. These can 
+be specified in pixels or as a percentage of 
+the containing element.
+
+
+Creating a Basic Table
+A basic HTML table is created using the <table> 
+tag. Inside the table, rows are defined using 
+the <tr> (table row) tag, and each row contains cells defined using either the <th> (table header) or <td> (table data) tags.
+
+*/
