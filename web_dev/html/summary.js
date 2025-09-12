4c5c982337c732302aca77cd9aed5d9385cae668
araborg <arababs2015@gmail.com>
2025-09-12 21:06:47 +0100

    septa training: 110

--- web_dev/html/summary.js
+++ web_dev/html/summary.js
@@ -7,8 +7,7 @@ Define the dimensions of the image. These can
 be specified in pixels or as a percentage of 
 the containing element.
 
-
-Creating a Basic Table
+Creating a Basic Table:
 A basic HTML table is created using the <table> 
 tag. Inside the table, rows are defined using 
 the <tr> (table row) tag, and each row contains 
