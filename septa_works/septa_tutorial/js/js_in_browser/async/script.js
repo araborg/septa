60c1b3225f37aba6481bf9bbbeca84825a92f707
araborg <arababs2015@gmail.com>
2025-11-01 15:00:02 +0100

    septa-js: 4

---
+++ septa_works/septa_tutorial/js/js_in_browser/async/script.js
@@ -0,0 +1,31 @@
+async function fetchUserData() {
+	try {
+		let response = await fetch(
+			"https://jsonplaceholder.typicode.com/users"
+		);
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch data");
+		}
+
+		let users = await response.json();
+
+		displayUsers(users);
+	} catch (error) {
+		console.error("Error:", error);
+	}
+}
+
+function displayUsers(users) {
+	const userList = document.getElementById("userList");
+
+	users.forEach((user) => {
+		let listItem = document.createElement("li");
+
+		listItem.textContent = `${user.name} (${user.email})`;
+
+		userList.appendChild(listItem);
+	});
+}
+
+fetchUserData();
