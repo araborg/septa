ac0d06e6d9b7ea0a9445d9db7497970e127a61f9
araborg <arababs2015@gmail.com>
2025-11-03 08:30:02 +0100

    septa_js: 4

--- septa_works/septa_tutorial/js/advanced_js/weather_app/weather.js
+++ septa_works/septa_tutorial/js/advanced_js/weather_app/weather.js
@@ -1,6 +1,8 @@
 document.getElementById("getWeather").addEventListener("click", async () => {
 	const city = document.getElementById("city").value;
+
 	const apiKey = "YOUR_API_KEY";
+
 	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 	try {
