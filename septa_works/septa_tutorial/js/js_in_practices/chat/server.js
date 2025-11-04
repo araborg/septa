46cc5abf1b2da1b0d9bd9f5173e8cb894de35571
araborg <arababs2015@gmail.com>
2025-11-04 20:34:02 +0100

    septa_js: 43

---
+++ septa_works/septa_tutorial/js/js_in_practices/chat/server.js
@@ -0,0 +1,21 @@
+// ds is a dummy server
+const WebSocket = require("ws");
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on("connection", (ws) => {
+	console.log("New client connected");
+
+	ws.on("message", (message) => {
+		console.log("Received:", message);
+
+		server.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(message);
+			}
+		});
+	});
+
+	ws.on("close", () => {
+		console.log("Client disconnected");
+	});
+});
