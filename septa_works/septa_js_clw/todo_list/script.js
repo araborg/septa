5ddc6907c7d7b07f3c19e5674065513be11b70ef
araborg <arababs2015@gmail.com>
2025-11-11 19:14:50 +0100

    septa_js_clw: todo_list 27

--- septa_works/septa_js_clw/todo_list/script.js
+++ septa_works/septa_js_clw/todo_list/script.js
@@ -8,8 +8,12 @@ const todoList = document.getElementById("todoList");
 addButton.addEventListener("click", addTask);
 
 function addTask() {
+	// if (todoInput.value.trim() === "") {}
+
 	// if d todoInput value is an empty string do nth
-	if (todoInput.value.trim() === "") {
-		console.log("Btn clicked");
-	}
+	if (!todoInput.value.trim()) return;
+
+	// cr8 an list, li and update it content
+	const li = document.createElement("li");
+	li.textContent = todoInput.value;
 }
