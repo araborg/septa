d20af160647172c97a2e1e639881dfa27e4cfcac
araborg <arababs2015@gmail.com>
2025-11-11 19:52:04 +0100

    septa_js_clw: todo_list 47

--- septa_works/septa_js_clw_practice/todo_list/script.js
+++ septa_works/septa_js_clw_practice/todo_list/script.js
@@ -2,3 +2,7 @@ const task = document.getElementById("task");
 const addTaskBtn = document.getElementById("addTask");
 
 const outputTasks = document.getElementById("output");
+
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {}
