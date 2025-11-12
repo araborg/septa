ed89770df95d21c64ddc495ce4d8bc979bf00f83
araborg <arababs2015@gmail.com>
2025-11-12 08:41:01 +0100

    septa_js: img_gallery: 16

--- septa_works/septa_js_clw/img_gallary/script.js
+++ septa_works/septa_js_clw/img_gallary/script.js
@@ -1,2 +1,14 @@
 const featured = document.getElementById("featured");
 const thumbnails = document.getElementById("thumbnails");
+
+thumbnails.addEventListener("click", handleClick);
+
+function handleClick(event) {
+	if (event.target.classList.contains("thumbnail")) {
+		featured.src = event.target.src.replace("100/100", "500");
+	}
+
+	featured.addEventListener("mouseenter", function () {
+		this.style.opacity = "0.7";
+	});
+}
