e333ba691dc0a8465f6eded047a728bb08ae4633
araborg <arababs2015@gmail.com>
2025-11-13 08:46:25 +0100

    septa_js: 23

--- septa_works/septa_js_clw_practice/img_gallary/script.js
+++ septa_works/septa_js_clw_practice/img_gallary/script.js
@@ -12,7 +12,24 @@ let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 */
 
+// select form
 const form = document.getElementById("registrationForm");
+
+// select inputs
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
+
+// select error spans
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+
+form.addEventListener("submit", function (event) {
+	event.preventDefault();
+
+	// clear previous errors
+	nameError.textContent = "";
+	emailError.textContent = "";
+	passwordError.textContent = "";
+});
