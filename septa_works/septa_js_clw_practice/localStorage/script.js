36497a644989d5bf4bca717ca790cb7cc49ea44a
araborg <arababs2015@gmail.com>
2025-11-15 20:55:33 +0100

    septa_js: clws 99

--- septa_works/septa_js_clw_practice/localStorage/script.js
+++ septa_works/septa_js_clw_practice/localStorage/script.js
@@ -2,3 +2,11 @@ const lightBtn = document.querySelector("#light");
 const darkBtn = document.querySelector("#dark");
 
 const body = document.querySelector("body");
+
+lightBtn.addEventListener("click", handleClick);
+
+function handleClick(e) {
+	e.preventDefault();
+
+	console.log(this);
+}
