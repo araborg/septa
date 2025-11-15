b9496c8ce2133f796ec8744143cfabc4f05dae82
araborg <arababs2015@gmail.com>
2025-11-15 10:19:17 +0100

    septa_js: 16

--- septa_works/septa_js_clw/localStorage/script.js
+++ septa_works/septa_js_clw/localStorage/script.js
@@ -1,2 +1,9 @@
 const ligthThemeButton = document.querySelector("lightTheme");
 const darkThemeButton = document.querySelector("darkTheme");
+
+// init and apply stored theme on pg load
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+	document.body.className = savedTheme;
+}
