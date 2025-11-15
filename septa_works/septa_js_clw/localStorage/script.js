3577f19da73e4d71c0c7d821ee596293063d56fe
araborg <arababs2015@gmail.com>
2025-11-15 10:21:03 +0100

    septa_js: 17

--- septa_works/septa_js_clw/localStorage/script.js
+++ septa_works/septa_js_clw/localStorage/script.js
@@ -7,3 +7,16 @@ const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
 	document.body.className = savedTheme;
 }
+
+// Event listeners for theme buttons
+ligthThemeButton.addEventListener("click", () => {
+	document.body.className = "light-theme";
+
+	localStorage.setItem("theme", "light-theme");
+});
+
+darkThemeButton.addEventListener("click", () => {
+	document.body.className = "dark-theme";
+
+	localStorage.setItem("theme", "dark-theme");
+});
