0b7b93f2073bed70d95674f713cd14ccfed8c2fa
araborg <arababs2015@gmail.com>
2025-11-15 12:53:45 +0100

    septa_js: clws 29

--- septa_works/septa_js_clw_practice/simple_reg_form/script.js
+++ septa_works/septa_js_clw_practice/simple_reg_form/script.js
@@ -7,3 +7,9 @@ let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const form = document.querySelector("form");
 
 console.log(form);
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit() {
+	console.log("Clicked");
+}
