eb1916fa0ea34f4cfeecaa22731dd777313df855
araborg <arababs2015@gmail.com>
2025-11-15 16:49:22 +0100

    septa_js: clws 65

--- septa_works/septa_js_clw/fetch_data/script.js
+++ septa_works/septa_js_clw/fetch_data/script.js
@@ -89,3 +89,11 @@ Using fetch():
 
 
 */
+
+fetch("https://api.example.com/data").then((response) => {
+	if (!response.ok) {
+		throw new Error("Network response was not ok");
+	}
+
+	return response.json();
+});
