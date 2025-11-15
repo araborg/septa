b756743c9fdc5ca3e8f6800f8a137b47235df191
araborg <arababs2015@gmail.com>
2025-11-15 22:05:54 +0100

    septa_js: clws 110

--- septa_works/septa_js_clw_practice/localStorage/script.js
+++ septa_works/septa_js_clw_practice/localStorage/script.js
@@ -1,5 +1,5 @@
-const lightBtn = document.querySelector("#light");
-const darkBtn = document.querySelector("#dark");
+const lightBtn = document.querySelector("#light-theme");
+const darkBtn = document.querySelector("#dark-theme");
 
 const hiddenPara = document.querySelector("#hidden");
 
@@ -7,8 +7,13 @@ lightBtn.addEventListener("click", handleClick.bind(this));
 darkBtn.addEventListener("click", handleClick.bind(this));
 
 function handleClick(arg) {
-	const selectedTheme = arg.target.id;
+	const selectedTheme = arg.target.id.split("-")[0];
 
 	hiddenPara.textContent = `Theme change to ${selectedTheme}`;
 	document.body.className = selectedTheme;
+
+	localStorage.setItem("saveTheme", arg.target.id);
 }
+
+const savedTheme = localStorage.getItem("saveTheme");
+console.log(savedTheme);
