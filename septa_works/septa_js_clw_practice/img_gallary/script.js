0c8ff242b8db8657dc7e72dce79215532114bf4f
araborg <arababs2015@gmail.com>
2025-11-15 12:33:22 +0100

    septa_js: clws 20

--- septa_works/septa_js_clw_practice/img_gallary/script.js
+++ septa_works/septa_js_clw_practice/img_gallary/script.js
@@ -1,12 +1,4 @@
 /*
-For revision: imgs links
-<img src="https://picsum.photos/id/1/500/500" alt="Featured Image" id="featured">
-<img src="https://picsum.photos/id/1/100/100" alt="Thumbnail 1" class="thumbnail">
-<img src="https://picsum.photos/id/2/100/100" alt="Thumbnail 2" class="thumbnail">
-<img src="https://picsum.photos/id/3/100/100" alt="Thumbnail 3" class="thumbnail">
-<img src="https://picsum.photos/id/4/100/100" alt="Thumbnail 4" class="thumbnail">
-
-
 Regex email pattern:
 let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
