c9b349dad98e0e7d448ff3110c6b13a0bc56bedf
araborg <arababs2015@gmail.com>
2025-11-16 13:11:43 +0100

    septa_js: clws 127

--- septa_works/septa_js_clw_practice/fetchData/script.js
+++ septa_works/septa_js_clw_practice/fetchData/script.js
@@ -1,25 +1,27 @@
 // "https://jsonplaceholder.typicode.com/users";
 
 const items = document.querySelector("#items");
-console.log(items);
 
 async function fetchUser() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/users");
+	try {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/users"
+		);
 
-	if (!response.ok) {
-		console.log("Response is currently unavailable");
+		if (!response.ok) {
+			console.log("Response is currently unavailable");
+		}
+
+		const users = await response.json();
+
+		updateUI(users);
+	} catch (error) {
+		console.error("Error: ", error);
 	}
-
-	const users = await response.json();
-
-	updateUI(users);
 }
 
 function updateUI(data) {
-	data.map((el) => {
-		const li = document.createElement("li");
-		li.textContent = el.name;
-	});
+	console.log(data);
 }
 
 fetchUser();
