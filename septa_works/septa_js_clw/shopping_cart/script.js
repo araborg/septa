a314099b52846e3bc75ee9b7e2c5f80bffa11af5
araborg <arababs2015@gmail.com>
2025-11-17 16:21:16 +0100

    septa_js: shopping_cart: 9

--- septa_works/septa_js_clw/shopping_cart/script.js
+++ septa_works/septa_js_clw/shopping_cart/script.js
@@ -13,6 +13,7 @@ let dog = {
 // set animal as d prototype of dog
 dog.__proto__ = animal;
 
+// both eats & sleep r inherited 4rm animal obj
 console.log(dog.eats); // true
 
 dog.sleep(); // The animal is sleeping
