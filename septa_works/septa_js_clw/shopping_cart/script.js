d2a1e62f6a5715247741677b201bac60b44c6dba
araborg <arababs2015@gmail.com>
2025-11-17 20:39:26 +0100

    septa_js: shopping_cart: 44

--- septa_works/septa_js_clw/shopping_cart/script.js
+++ septa_works/septa_js_clw/shopping_cart/script.js
@@ -121,4 +121,36 @@ class Item {
 	}
 }
 
-class ShoppingCart {}
+class ShoppingCart {
+	constructor() {
+		this.items = [];
+	}
+
+	addItem(item) {
+		this.items.push(item);
+
+		this.displayCart();
+	}
+
+	displayCart() {
+		const cartItems = document.getElementById("cartItems");
+
+		cartItems.innerHTML = "";
+
+		this.items.forEach((item) => {
+			let listItem = document.createElement("li");
+
+			listItem.textContent = item.display();
+
+			cartItems.appendChild(listItem);
+		});
+	}
+}
+
+const cart = new ShoppingCart();
+
+document.getElementById("addItem").addEventListener("click", () => {
+	let newItem = new Item("Apple", 0.99);
+
+	cart.addItem(newItem);
+});
