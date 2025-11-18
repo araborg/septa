6e3dc39eec621d34c340be898239904a606cb158
araborg <arababs2015@gmail.com>
2025-11-18 21:33:01 +0100

    septa_js: js modules: 5

--- septa_works/septa_js_clw/js_module/script.js
+++ septa_works/septa_js_clw/js_module/script.js
@@ -1,12 +1,15 @@
 // Function Expressions:
 const myModule = (function () {
+	// declare private variable
 	let privateVariable = "I am private";
 
+	// use d private variable inside a fxn
 	function privateMethod() {
 		console.log(privateVariable);
 	}
 
 	return {
+		// access d private method inside anoda method
 		publicMethod: function () {
 			privateMethod();
 		},
