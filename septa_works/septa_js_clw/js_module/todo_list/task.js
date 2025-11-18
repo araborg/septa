04a70f0da214df131bbfa77855e443bae39b0d07
araborg <arababs2015@gmail.com>
2025-11-18 22:01:43 +0100

    septa_js: js modules: 16

---
+++ septa_works/septa_js_clw/js_module/todo_list/task.js
@@ -0,0 +1,11 @@
+export class Task {
+	constructor(description) {
+		this.description = description;
+
+		this.completed = false;
+	}
+
+	toggleComplete() {
+		this.completed = !this.completed;
+	}
+}
