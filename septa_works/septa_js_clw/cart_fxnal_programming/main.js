c59dc9c4336ca51e74109c78b3247ebdddd0cbc2
araborg <arababs2015@gmail.com>
2025-11-19 13:20:57 +0100

    septa_js: js modules: 81

--- septa_works/septa_js_clw/cart_fxnal_programming/main.js
+++ septa_works/septa_js_clw/cart_fxnal_programming/main.js
@@ -4,6 +4,8 @@ let cart = [];
 
 cart = addItem(cart, { name: "Apple", price: 0.99 });
 cart = addItem(cart, { name: "Banana", price: 1.29 });
+cart = addItem(cart, { name: "Cashew", price: 2.0 });
+cart = addItem(cart, { name: "Banana", price: 4.91 });
 
 console.log({ cart });
 
