8d220f7ea0169fd23594b6b4c2232a9900d0ac40
araborg <arababs2015@gmail.com>
2025-11-19 13:10:15 +0100

    septa_js: js modules: 75

--- septa_works/septa_js_clw/cart_fxnal_programming/cart.js
+++ septa_works/septa_js_clw/cart_fxnal_programming/cart.js
@@ -21,3 +21,6 @@ const addItem = (cart, item) => [...cart, item];
 
 const removeItem = (cart, itemName) =>
 	cart.filter((item) => item.name !== itemName);
+
+const calculateTotal = (cart) =>
+	cart.reduce((total, item) => total + item.price, 0);
