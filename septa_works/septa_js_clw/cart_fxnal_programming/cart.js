2a684725267113f0ff85c6c4175f2bc69442653f
araborg <arababs2015@gmail.com>
2025-11-19 13:00:24 +0100

    septa_js: js modules: 68

---
+++ septa_works/septa_js_clw/cart_fxnal_programming/cart.js
@@ -0,0 +1,18 @@
+/*
+
+
+*/
+
+function makeCounter() {
+	let count = 0;
+
+	return function () {
+		count++;
+
+		return count;
+	};
+}
+
+const counter = makeCounter();
+
+console.log({ counter });
