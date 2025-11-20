a4de0cba8f72dca24f530dd12dbfb78d78086932
araborg <arababs2015@gmail.com>
2025-11-20 08:58:48 +0100

    septa_js: 9

--- septa_works/septa_js_clw/4/api/data_fetching/script.js
+++ septa_works/septa_js_clw/4/api/data_fetching/script.js
@@ -41,3 +41,12 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 	.catch((error) => console.error("Error: ", error));
 
 // Making POST Requests:
+// fetch("https://jsonplaceholder.typicode.com/posts", {})
+// fetch("https://jsonplaceholder.typicode.com/posts", {method, headers, body})
+fetch("https://jsonplaceholder.typicode.com/posts", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+	},
+	body: JSON.stringify(),
+});
